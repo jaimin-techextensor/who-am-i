af1f8e10355f963c0507d7981dfb0bb51d2fd5db
Jaimin <jaimin.shah@energydatametrics.com>
2024-08-05 16:51:46 +0530

    Add project files.

---
+++ Controllers/WhoAmI.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WhoAmI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WhoAmI : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public WhoAmI(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet()]
+        public IActionResult Index()
+        {
+            var ipAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+            var language = Request.Headers["Accept-Language"].ToString().Split(',')[0];
+            var software = $"{System.Runtime.InteropServices.RuntimeInformation.OSDescription} {System.Runtime.InteropServices.RuntimeInformation.OSArchitecture}";
+
+            var response = new
+            {
+                ipaddress = ipAddress,
+                language = language,
+                software = software
+            };
+
+            return Ok(response);
+        }
+    }
+}
